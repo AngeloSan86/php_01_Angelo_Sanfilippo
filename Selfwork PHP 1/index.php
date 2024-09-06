7d99cff2ee7667b9f678fd79efcea7446216fcb4
Angelo Sanfilippo <124434790+AngeloSan86@users.noreply.github.com>
2024-09-06 14:55:00 +0200

    Add files via upload

---
+++ Selfwork PHP 1/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$int = 1;  // dato Integer
+$float = 0.5; // dato Float
+$string = "Angelo"; // dato Stringa
+$boool = true; // dato Boolean
+
+var_dump($int);
+var_dump($float);
+var_dump($string);
+var_dump($boool);
+
+const NUM_INTERO = 1;
+const NUM_FLOAT = 0.5;
+const STRINGA = "ANGELO";
+const BOOLEANA = true;
+
+var_dump(NUM_INTERO);
+var_dump(NUM_FLOAT);
+var_dump(STRINGA);
+var_dump(BOOLEANA);
+
+?>
